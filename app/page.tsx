c27d2c8fe94c1695832f1a6d599b70761aceca50
newbee1939 <hideto3970@gmail.com>
2023-09-11 08:43:50 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default async function Home() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        marginTop: "200px",
       }}
     >
       <ClientComponent />
